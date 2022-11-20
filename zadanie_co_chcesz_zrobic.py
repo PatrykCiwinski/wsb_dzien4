7f2171b78b143305e8c915fab3980e821c83549a
Patryk <patryk.ciwinski@gmail.com>
2022-11-20 14:41:29 +0100

    change to oop

--- zadanie_co_chcesz_zrobic.py
+++ zadanie_co_chcesz_zrobic.py
@@ -6,17 +6,16 @@ def wypisz_menu():
     print('4. Usuń rzecz do zrobienia')
     print('5. Ustaw konkretną rzecz na początek')
     print('6. Wyjdź z programu')
-
-
+    print('7. Ocznacz zrobioną rzecz jako niezrobioną')
 def pobierz_opcje_do_zrobienia():
     while True:
-        i = input('Co chcesz zrobić? (1-6): ')
+        i = input('Co chcesz zrobić? (1-7): ')
         try:
             liczba = int(i)
         except ValueError:
             print('To musi być liczba')
             continue
-        if 1 <= liczba <= 6:
+        if 1 <= liczba <= 7:
             return liczba
         print('To musi być liczba od 1 do 6')
 class ListaRzeczyDoZrobienia:
@@ -26,7 +25,7 @@ class ListaRzeczyDoZrobienia:
     def dodaj_rzecz_do_zrobienia(self):
         self.lista_rzeczy_do_zrobienia.append(input('Podaj rzecz do zrobienia: '))
     def oznacz_rzecz_jako_zrobiona(self):
-        indeks_zrobionej_rzeczy = self._pobierz_indeks_rzeczy_do_zrobienia()
+        indeks_zrobionej_rzeczy = self._pobierz_indeks()
         self.lista_rzeczy_zrobionych.append(self.lista_rzeczy_do_zrobienia[indeks_zrobionej_rzeczy])
         del self.lista_rzeczy_do_zrobienia[indeks_zrobionej_rzeczy]
     def pokaz_rzeczy_do_zrobienia(self):
@@ -39,17 +38,19 @@ class ListaRzeczyDoZrobienia:
         for indeks, rzeczy_do_zrobienia in enumerate(self.lista_rzeczy_zrobionych):
             print(f'\t\t{indeks} [x] {rzeczy_do_zrobienia}')
     def usun_rzecz_do_zrobienia(self):
-        indeks_rzeczy_do_usuniecia = self._pobierz_indeks_rzeczy_do_zrobienia()
+        indeks_rzeczy_do_usuniecia = self._pobierz_indeks()
         del self.lista_rzeczy_do_zrobienia[indeks_rzeczy_do_usuniecia]
     def ustaw_rzecz_na_poczatek(self):
-        indeks_zrobionej_rzeczy = self._pobierz_indeks_rzeczy_do_zrobienia()
+        indeks_zrobionej_rzeczy = self._pobierz_indeks()
         element_na_poczatek = self.lista_rzeczy_do_zrobienia[indeks_zrobionej_rzeczy]
         del self.lista_rzeczy_do_zrobienia[indeks_zrobionej_rzeczy]
         self.lista_rzeczy_do_zrobienia.insert(0, element_na_poczatek)
-    def _pobierz_indeks_rzeczy_do_zrobienia(self):
+    def _pobierz_indeks(self, lista=None):
+        if lista is None:
+            lista = self.lista_rzeczy_do_zrobienia
         while True:
             print()
-            for indeks, rzeczy_do_zrobienia in enumerate(self.lista_rzeczy_do_zrobienia):
+            for indeks, rzeczy_do_zrobienia in enumerate(lista):
                 print(f'\t\t{indeks} [ ] {rzeczy_do_zrobienia}')
             print()
             try:
@@ -57,10 +58,14 @@ class ListaRzeczyDoZrobienia:
             except ValueError:
                 print('to musi być liczba')
                 continue
-            if indeks_rzeczy >= 0 and indeks_rzeczy < len(self.lista_rzeczy_do_zrobienia):
+            if indeks_rzeczy >= 0 and indeks_rzeczy < len(lista):
                 return indeks_rzeczy
             else:
                 print('podana liczba jest spoza przedzialu')
+    def oznacz_rzecz_zrobiona_jako_niezrobiona(self):
+        indeks_zrobionej_rzeczy = self._pobierz_indeks(self.lista_rzeczy_zrobionych)
+        self.lista_rzeczy_do_zrobienia.append(self.lista_rzeczy_zrobionych[indeks_zrobionej_rzeczy])
+        del self.lista_rzeczy_zrobionych[indeks_zrobionej_rzeczy]
 def petla_glowna_programu():
     instancje_rzeczy_do_zrobienia = ListaRzeczyDoZrobienia()
     while True:
@@ -78,4 +83,6 @@ def petla_glowna_programu():
             instancje_rzeczy_do_zrobienia.ustaw_rzecz_na_poczatek()
         elif opcja == 6:
             return
+        elif opcja == 7:
+            instancje_rzeczy_do_zrobienia.oznacz_rzecz_zrobiona_jako_niezrobiona()
 petla_glowna_programu()
